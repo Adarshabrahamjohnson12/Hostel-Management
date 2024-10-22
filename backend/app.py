d49bd60b62d76ebe0cd2d9a4d5dfe6faf1dc2691
Alwin-Sajan <alwinsajan225@gmail.com>
2024-10-22 09:07:25 +0530

    update the api for student details

--- backend/app.py
+++ backend/app.py
@@ -114,9 +114,58 @@ def userdetails():
             }), 400
 
     except Exception as e:
-        return jsonify({"error": str(e)}), 500 
+        return jsonify({"error": str(e)}), 500
+
+#to get room details  
+@app.route("/roomdetails",methods=['POST'])   
+def roomdetails():
+    try:
+        data = request.json
+        Hno = data.get('Hno')
+        
+        if not Hno:
+            return jsonify({
+                "statusDesc": "Failure",
+                "statusCode": {"code": "F001"},
+                "message": 'Hostel id is required',
+                "login": False
+            }), 400
+        
+        db_connection = get_db_coneection()
+        cursor = db_connection.cursor()
+        
+        # Use parameterized query to prevent SQL injection
+        cursor.execute('SELECT Rono, full_name , Floor FROM `room_data` as x,`user_data` as y where x.Hno = y.Hno AND Rono = (SELECT Rono FROM `room_data` WHERE Hno = %s);', (Hno,))
+        row = cursor.fetchone()
+        db_connection.close()
+        
+        rono = row[0][0]
+        floor = row[0][2]
+        roomate = []
+        for i in row:
+            roomate.append(row[i][1])
+            
+        if row:  # If a user was found
+            return jsonify({
+                "statusDesc": "Success",
+                "statusCode": {"code": "SC000"},
+                "message": "Got details sucess",
+                "Rono": rono,
+                "roomate" : roomate,
+                "floor": floor
+            }), 200
+        else:
+            return jsonify({
+                "statusDesc": "Failure",
+                "statusCode": {"code": "F005"},
+                "message": 'User does not exist'
+            }), 400
+        
+        
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
     
- 
  
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5080,debug=True )
--- frontend/src/components/RoomDetails.jsx
+++ frontend/src/components/RoomDetails.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const RoomDetails = ({userData}) => {
-  const username = userData.email;
+  const username = userData.Hno;
+
+  
 
   
   return (
